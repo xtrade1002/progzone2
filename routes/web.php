54d428c2e02a749d3304e8021dde25c1e86fe8cb
xtrade1002 <165554747+xtrade1002@users.noreply.github.com>
2025-09-16 20:48:15 +0200

    Add named routes for menu links

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::view('/', 'welcome')->name('home');
+
+Route::view('/about-me', 'welcome')->name('aboutme');
+Route::view('/services', 'welcome')->name('services');
+Route::view('/prices', 'welcome')->name('prices');
+Route::view('/references', 'welcome')->name('references');
+Route::view('/studies', 'welcome')->name('studies');
+Route::view('/contact', 'welcome')->name('contact');
+
+Route::view('/privacy', 'welcome')->name('privacy');
+Route::view('/terms', 'welcome')->name('terms');
+Route::view('/impressum', 'welcome')->name('impressum');
