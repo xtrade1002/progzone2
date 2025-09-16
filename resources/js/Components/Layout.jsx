7a848a520102a0ff22fb3a75d8aba89192a952fb
xtrade1002 <165554747+xtrade1002@users.noreply.github.com>
2025-09-16 22:21:15 +0200

    Update layout structure

--- resources/js/Components/Layout.jsx
+++ resources/js/Components/Layout.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import FooterMenu from './FooterMenu.jsx';
+import Footer from './FooterMenu.jsx';
 import MainMenu from './MainMenu.jsx';
 
 export default function Layout({ activePath, children }) {
   return (
-    <div className="min-h-screen bg-[#101010] text-white flex flex-col">
+    <div className="min-h-screen text-[#FF007A] flex flex-col">
       <MainMenu activePath={activePath} />
-      <main className="flex-1 w-full">{children}</main>
-      <FooterMenu />
+      <main className="flex-grow flex items-center justify-center">{children}</main>
+      <Footer />
     </div>
   );
 }
