f7839aea3288c60db051ca99819ac7edd9afe1a6
xtrade1002 <165554747+xtrade1002@users.noreply.github.com>
2025-09-17 21:37:19 +0200

    Fix inertia page resolution and remove unused React refresh

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -3,9 +3,9 @@ import { createRoot } from 'react-dom/client';
 import { createInertiaApp } from '@inertiajs/react';
 
 createInertiaApp({
-  resolve: name => {
-    const pages = import.meta.glob('./Pages/**/*.jsx', { eager: true });
-    return pages[`./Pages/${name}.jsx`];
+  resolve: (name) => {
+    const pages = import.meta.glob('./pages/**/*.jsx', { eager: true });
+    return pages[`./pages/${name}.jsx`];
   },
   setup({ el, App, props }) {
     createRoot(el).render(<App {...props} />);
