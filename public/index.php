967f4f037021c0f6232335629639be8c3b796fab
xtrade1002 <165554747+xtrade1002@users.noreply.github.com>
2025-09-26 12:30:47 +0200

    up

--- public/index.php
+++ public/index.php
@@ -5,16 +5,16 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
-// Determine if the application is in maintenance mode...
-if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
-    require $maintenance;
+// Ha a rendszer karbantartásban van
+if (file_exists(__DIR__.'/storage/framework/maintenance.php')) {
+    require __DIR__.'/storage/framework/maintenance.php';
 }
 
-// Register the Composer autoloader...
-require __DIR__.'/../vendor/autoload.php';
+// Composer autoload
+require __DIR__.'/vendor/autoload.php';
 
-// Bootstrap Laravel and handle the request...
+// Bootstrap Laravel és kezeld a kérést
 /** @var Application $app */
-$app = require_once __DIR__.'/../bootstrap/app.php';
+$app = require_once __DIR__.'/bootstrap/app.php';
 
 $app->handleRequest(Request::capture());
